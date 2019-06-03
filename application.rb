169a3dd679229ccdf0fe4178b1e745919ff6f9f6
James Cox <jamesncox@yahoo.com>
2019-06-03 16:46:12 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is James! I have just built my very own website from scratch! BOOYAH BABY!"
     resp.finish
   end
 
