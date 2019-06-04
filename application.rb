23615fc36b528241c325f784480b1485b1a081db
Mark Kanko <mkanko66@gmail.com>
2019-06-04 08:44:32 -0700

    completed inspecting-the-web-with-rack

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Spartacus. oiLKNMEFLS;JFDSVNSJKHV;IK.KJSDF/LVS;KJVHD;KJFVKJN/KJHSDKDHFVK/JHNVDKHSDKJHKSJHVK.SJHNV.KSJHD.KJHSN.KDVJSKD.JHNVSK.DJHV.KSJDHNV.KJSDHNV.KJSDK.JSD.JVHSND.KJVHNS.KJDHNK.SJDHNCV.KSJNDCV.KSJHND.KSJDHN.KJSHVNDKJSHNCVKJSAhnDCKJShdCVJHSndKVC.JHNSD.KJVCHS.KDJHNV.KJN.XNC.MCXHN YHI;UHKJFNkjhnkjfkjfdiijfkjowiuhkdfkljdljflkhjdkjhd;iuf  jkhd;kfjh;kaghf;ihvuevkjhnvkjn;kjh;ksjhdf;uiofvejrknuhsjdhksjdhffkjsdkjhdfksdh;sd;kjsdkfjhs;kjfhskjahdfk;sdhfk;sjhdfk;jsdljsdalkfjhsalkdjfhaksljdhfkjsadhfksajdhfk"
     resp.finish
   end
 
